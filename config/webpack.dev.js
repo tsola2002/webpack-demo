36b6c324d27bb97c2657c8ca530d94c24d4b3d3a
Omatsola Sobotie <tsola2002@yahoo.co.uk>
2018-10-16 11:38:06 +0100

    implemented babel polyfill, presets, & transforms

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,8 +3,7 @@ const path = require("path")
 
 module.exports = {
     entry: {
-        main: ["core-js/fn/promise",
-            "./src/main.js"]
+        main: ["./src/main.js"]
     },
     mode: "development",
     output: {
